e8600446594a5ee6d1242aaec1d94d4127e081b2
Riad Krim <riad.krim@outlook.com>
2021-10-31 22:49:30 +0100

    Update macOS defaults
    Surround properties with quotation mark.
    Add Mission Control Hot Corners
    Silence killall command when looked up process is not running

--- macos_defaults.sh
+++ macos_defaults.sh
@@ -19,7 +19,7 @@ function close_application() {
   return $?
 }
 
-# ToDo:
+# To-Do:
 # Should take application_name and config, finds the bundle_id and apply config
 # How to deal with config and global domain?
 # function write_config() {
@@ -44,44 +44,65 @@ bundle_id=`bundle_id "$application_name"`
 
 
 # Desktop
-defaults write $bundle_id ShowExternalHardDrivesOnDesktop -bool true
-defaults write $bundle_id ShowHardDrivesOnDesktop -bool true
-defaults write $bundle_id ShowMountedServersOnDesktop -bool true
-defaults write $bundle_id ShowRemovableMediaOnDesktop -bool true
+defaults write $bundle_id "ShowExternalHardDrivesOnDesktop" -bool true
+defaults write $bundle_id "ShowHardDrivesOnDesktop" -bool true
+defaults write $bundle_id "ShowMountedServersOnDesktop" -bool true
+defaults write $bundle_id "ShowRemovableMediaOnDesktop" -bool true
 
 # Window Presentation
-defaults write $bundle_id FXDefaultSearchScope -string "SCcf"
-defaults write $bundle_id _FXSortFoldersFirst -bool true
-defaults write $bundle_id ShowStatusBar -bool true
+defaults write $bundle_id "FXDefaultSearchScope" -string "SCcf"
+defaults write $bundle_id "_FXSortFoldersFirst" -bool true
+defaults write $bundle_id "ShowStatusBar" -bool true
 
 # Not Working 😔
 # defaults write $bundle_id ShowPathbar -bool true
 # defaults write $bundle_id QLEnableTextSelection -bool true
 
-killall $application_name
+killall $application_name 2> /dev/null
 
 
 ### Dock #######################################################################
 application_name="Dock"
 bundle_id=`bundle_id "$application_name"`
 
+# Position on screen
+defaults write $bundle_id "orientation" -string "bottom"
+
+# Icon size
+defaults write $bundle_id "tilesize" -int 48
+
 # Auto-hiding the dock
-defaults write $bundle_id autohide -bool true
-defaults write $bundle_id autohide-delay -float 0
+defaults write $bundle_id "autohide" -bool true
+defaults write $bundle_id "autohide-delay" -float 0.0001
+defaults write $bundle_id "autohide-time-modifier" -float 0.5
 
-# Minimiza windows into applicatin icon
-defaults write $bundle_id minimize-to-application -bool true
+# Minimize windows into application icon
+defaults write $bundle_id "minimize-to-application" -bool true
+defaults write $bundle_id "mineffect" -string "genie"
 
-# Remove Show recent applications in Dock
-defaults write $bundle_id show-recents -bool false
+# Do not show recent applications in Dock
+defaults write $bundle_id "show-recents" -bool false
 
-# Mission Control
-defaults write $bundle_id expose-group-apps -bool true
+# Bounce animation on app launch
+defaults write $bundle_id "enable-spring-load-actions-on-all-items" -bool true
+
+# Mission Control; Exposé, group apps instances
+defaults write $bundle_id "expose-group-apps" -bool true
 
 # Disable automatic Spaces rearrange based on recent use
-defaults write $bundle_id mru-spaces -bool false
+defaults write $bundle_id "mru-spaces" -bool false
 
-killall $application_name
+# Hot corners: top-left; top-right; bottom-left; bottom-right
+defaults write $bundle_id "wvous-tl-corner" -int 0  # Disabled
+defaults write $bundle_id "wvous-tr-corner" -int 13 # Lock Screen
+defaults write $bundle_id "wvous-bl-corner" -int 4  # Desktop
+defaults write $bundle_id "wvous-br-corner" -int 5  # Start Screen Saver
+
+# Wipe all apps from Dock
+#defaults write $bundle_id "persistent-apps" -array
+#defaults write $bundle_id "persistent-others" -array
+
+killall $application_name 2> /dev/null
 
 
 ### Safari #####################################################################
@@ -96,18 +117,24 @@ defaults write $bundle_id NewTabBehavior -int 1
 defaults write $bundle_id ShowFavoritesBar-v2 -bool true
 defaults write $bundle_id OpenNewTabsInFront -bool false
 defaults write $bundle_id IncludeDevelopMenu -bool true
-defaults write $bundle_id IncludeInternalDebugMenu -bool true
-defaults write $bundle_id ShowIconsInTabs -bool true
+
+
 defaults write $bundle_id ShowAllItemsInReadingList -bool true
-defaults write $bundle_id ShowFullURLInSmartSearchField -bool true
 defaults write $bundle_id ShowIconsInTabs -bool true
 defaults write $bundle_id ShowSidebarInNewWindows -bool false
 defaults write $bundle_id ShowSidebarInTopSites -bool false
-defaults write $bundle_id AutoOpenSafeDownloads -bool false
 defaults write $bundle_id HistoryAgeInDaysLimit -int 365
 
-killall $application_name
+# Enable Debug menu
+defaults write $bundle_id "IncludeInternalDebugMenu" -bool true
 
+# Disable safe file automatically open
+defaults write $bundle_id "AutoOpenSafeDownloads" -bool false
+
+# Show full URL in address bar
+defaults write $bundle_id "ShowFullURLInSmartSearchField" -bool true
+
+killall $application_name 2> /dev/null
 
 ### Developer ##################################################################
 
@@ -116,7 +143,7 @@ bundle_id=`bundle_id "$application_name"`
 
 defaults write $bundle_id AllowFullscreenMode -bool true
 
-killall $application_name
+killall $application_name 2> /dev/null
 
 
 
@@ -155,3 +182,14 @@ defaults write com.apple.ImageCapture disableHotPlug -bool true
 
 defaults write com.apple.TextEdit RichText -int 0
 defaults write com.apple.reminders RemindersDebugMenu -boolean true
+
+
+### Other ######################################################################
+
+# Don't create '.DS_Store' files on network and USB volumes
+defaults write com.apple.desktopservices "DSDontWriteNetworkStores" -bool true
+defaults write com.apple.desktopservices "DSDontWriteUSBStores" -bool true
+
+# CoreFoundation Preferences Deamon (cfprefsd -- defaults server)
+# Preferences values are cached, need to restart the server to apply changes
+killall cfprefsd
