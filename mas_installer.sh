d572b22db54e3c83e703a53812a0ccf9c69954c1
Riad Krim <riad.krim@outlook.com>
2023-04-26 02:58:36 +0200

    Add 'Hidden Bar' Application

--- mas_installer.sh
+++ mas_installer.sh
@@ -31,31 +31,36 @@ mas_apps=(
 # "1333542190"  # 1Password 7 | Install 1Password 8 from website
 
   # Developer
-  "497799835"   # Xcode
+	"640199958"   # Apple Developer
   "1496833156"  # Swift Playgrounds
-  "640199958"   # Apple Developer
   "1450874784"  # Transporter
+	"497799835"   # Xcode
 
   # Productivity
+	"409183694"   # Keynote
+	"1274495053"  # Microsoft To-Do
   "409203825"   # Numbers
   "409201541"   # Pages
-  "409183694"   # Keynote
+	"1278508951"  # Trello (rosetta)
 
   # Reference
   "430255202"   # MacTracker
 
   # Communication
+	"1480068668"  # Messenger
   "1176895641"  # Spark – Email App by Readdle
   "1482454543"  # Twitter
 # "803453959"   # Slack | Install from website
 
-  # Media Editor
+  # Media
   "408981434"   # iMovie
+	"461369673"   # Vox-Mp3-Flac-Music-Player
 
   # Safari Extensions
   "1440147259"  # AdGuard-For-Safari
 
   # Customization
+	"1452453066" 	# "Hidden Bar"
   "1284863847"  # Unsplash Wallpapers
 )
 mas_batch_install mas_apps[@]
@@ -63,14 +68,10 @@ mas_batch_install mas_apps[@]
 
 rosetta_apps=(
   "935235287"   # Encrypto-Secure-Your-Files (rosetta)
-  "1274495053"  # Microsoft-To-Do (rosetta)
-  "1278508951"  # Trello (rosetta)
+	"407963104"   # Pixelmator-Classic (rosetta)
   "568494494"   # Pocket (rosetta)
-  "1480068668"  # Messenger (rosetta)
-  "1147396723"  # WhatsApp Desktop (rosetta)
-  "461369673"   # Vox-Mp3-Flac-Music-Player (rosetta)
-  "407963104"   # Pixelmator-Classic (rosetta)
   "512204619"   # Radiant Defense (rosetta)
+	"1147396723"  # WhatsApp Desktop (rosetta)
 )
 
 if [ "$ARCH_NAME" == "$ARCH_X86_64" ]; then
